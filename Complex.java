2e04824485590bcbb2e0b025ccaa603028e6a26a
Trover <74741960+Troversones@users.noreply.github.com>
2024-02-26 12:20:55 +0100

    Complexddd
    hehehaha

---
+++ Complex.java
@@ -0,0 +1,39 @@
+public class Complex {
+	public toString() {
+		
+	}
+	protected double real(){
+		
+	}
+	protected double imag() {
+		
+	}
+	
+	public Complex(double real, double imag) {
+		
+	}
+	
+	public Complex() {
+		
+	}
+	
+	public String getReal() {
+		
+	}
+	
+	public getImag() {
+		
+	}
+	
+	public Complex add(Complex masik) {
+		return masik;
+	}
+	
+	public Complex mul(Complex masik) {
+		return masik;
+	}
+	
+	public Complex conjugate(){
+		
+	}
+}
